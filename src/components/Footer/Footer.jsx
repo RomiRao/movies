dce3252fad0e5bd15dd55a20e3f2c8503f7ad3f5
RomiRao <raoromina96@gmail.com>
2024-01-12 17:14:46 -0300

    well, that was an easy footer, see you in the next commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,16 @@
+import { Box, Typography } from "@mui/material";
+import { MdLocalMovies } from "react-icons/md";
+
 export default function Footer() {
-    return <div>Footer</div>;
+    return (
+        <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            sx={{ backgroundColor: "#555166", padding: 3, color: "white" }}
+        >
+            <MdLocalMovies />
+            <Typography mx={1}>Movie app</Typography>
+        </Box>
+    );
 }
