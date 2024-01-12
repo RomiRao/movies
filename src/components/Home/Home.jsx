7f20ff3ab41918a566fd68b689a6e207a544253f
RomiRao <raoromina96@gmail.com>
2024-01-12 19:06:08 -0300

    displaying home elements

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,3 +1,15 @@
+import { Container } from "@mui/material";
+
 export default function Home() {
-    return <div>Home</div>;
+    return (
+        <>
+            <h1>Carrousel</h1>
+            <Container
+                sx={{ display: "flex", justifyContent: "space-between" }}
+            >
+                <div>Box1</div>
+                <div>Box2</div>
+            </Container>
+        </>
+    );
 }
