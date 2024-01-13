05ecf1667f82596f9beed9c1c68dbe02bc4655f6
RomiRao <raoromina96@gmail.com>
2024-01-13 16:22:22 -0300

    adding dots slider on bottom of the image, still need to fix timeout function

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,11 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import MovieSmallList from "./MovieSmallList";
 import Carrousel from "./Carrousel";
+import { useState } from "react";
+import { GoDotFill, GoDot } from "react-icons/go";
 
 export default function Home() {
+    const [currentIndex, setCurrentIndex] = useState(0);
     const images = [
         {
             src: "https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/1ecde018e863e2aaee31f00a23378c35.jpe",
@@ -21,16 +24,54 @@ export default function Home() {
         },
     ];
 
+    const goNext = () => {
+        setTimeout(() => {
+            if (currentIndex < images.length - 1) {
+                setCurrentIndex(currentIndex + 1);
+            } else if (currentIndex === images.length - 1) {
+                setCurrentIndex(0);
+            }
+            console.log("timeout", currentIndex);
+        }, 5000);
+        clearTimeout(setTimeout);
+    };
+
+    goNext();
+
+    const goToIndex = (index) => {
+        setCurrentIndex(index);
+        console.log("index", index, currentIndex);
+    };
+
     return (
         <>
-            {images.map((movie) => (
+            <Box>
                 <Carrousel
-                    key={movie.src}
-                    title={movie.alt}
-                    desc={movie.desc}
-                    img={movie.src}
-                />
-            ))}
+                    title={images[currentIndex].alt}
+                    desc={images[currentIndex].desc}
+                    img={images[currentIndex].src}
+                    movies={images}
+                >
+                    <Box
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center"
+                    >
+                        {images.map((movie, index) => (
+                            <Box key={index}>
+                                {index === currentIndex ? (
+                                    <GoDotFill color="white" fontSize="20px" />
+                                ) : (
+                                    <GoDot
+                                        color="white"
+                                        onClick={() => goToIndex(index)}
+                                    />
+                                )}
+                            </Box>
+                        ))}
+                    </Box>
+                </Carrousel>
+            </Box>
             <Box
                 padding={8}
                 sx={{
