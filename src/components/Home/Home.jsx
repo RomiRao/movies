6e2a08c381b0046f4a6e84c156cc0e58d04a1cf1
RomiRao <raoromina96@gmail.com>
2024-01-13 14:45:51 -0300

    bringing example references to home and map them with carrousel component, and yes i know its spell carousel, im sorry

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,9 +3,34 @@ import MovieSmallList from "./MovieSmallList";
 import Carrousel from "./Carrousel";
 
 export default function Home() {
+    const images = [
+        {
+            src: "https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/1ecde018e863e2aaee31f00a23378c35.jpe",
+            alt: "one piece",
+            desc: "asdfasdagas",
+        },
+        {
+            src: "https://i.blogs.es/259581/one-piece/1366_2000.jpeg",
+            alt: "one piece",
+            desc: "asdasdas",
+        },
+        {
+            src: "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2018/08/one-piece_0.jpg?tf=3840x",
+            alt: "one piece",
+            desc: "asdasdas",
+        },
+    ];
+
     return (
         <>
-            <Carrousel />
+            {images.map((movie) => (
+                <Carrousel
+                    key={movie.src}
+                    title={movie.alt}
+                    desc={movie.desc}
+                    img={movie.src}
+                />
+            ))}
             <Box
                 padding={8}
                 sx={{
