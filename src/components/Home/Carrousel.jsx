4cae4f4903bd40b7afb890bafc4bd1f304c3937e
RomiRao <raoromina96@gmail.com>
2024-01-13 14:42:51 -0300

    setting propertys on carousel component

--- src/components/Home/Carrousel.jsx
+++ src/components/Home/Carrousel.jsx
@@ -1,24 +1,6 @@
 import { Box, Button, Typography } from "@mui/material";
 
-export default function Carrousel() {
-    const images = [
-        {
-            src: "https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/1ecde018e863e2aaee31f00a23378c35.jpe",
-            alt: "one piece",
-            desc: "asdfasdagas",
-        },
-        {
-            src: "https://i.blogs.es/259581/one-piece/1366_2000.jpeg",
-            alt: "one piece",
-            desc: "asdasdas",
-        },
-        {
-            src: "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2018/08/one-piece_0.jpg?tf=3840x",
-            alt: "one piece",
-            desc: "asdasdas",
-        },
-    ];
-
+export default function Carrousel({ title, img, desc, id }) {
     return (
         <Box
             display="flex"
@@ -28,7 +10,7 @@ export default function Carrousel() {
             width="100%"
             height={400}
             sx={{
-                backgroundImage: `url(${images[0].src})`,
+                backgroundImage: `url(${img})`,
                 backgroundPosition: "center",
                 backgroundSize: "cover",
             }}
@@ -45,10 +27,10 @@ export default function Carrousel() {
                 }}
             >
                 <Typography variant="h5" m={2}>
-                    {images[0].alt}
+                    {title}
                 </Typography>
                 <Typography m={1} variant="body2">
-                    {images[0].desc}
+                    {desc}
                 </Typography>
                 <Button m={1} variant="contained" size="small">
                     Ver mas...
