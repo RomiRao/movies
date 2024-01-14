d4270a7c599b1a1808b6b90e066fe70082e0ae09
RomiRao <raoromina96@gmail.com>
2024-01-14 13:35:05 -0300

    search structure done, lets do some funct before you blame me cuz its horrible now jiji

--- src/components/Search/MoviesSearch.jsx
+++ src/components/Search/MoviesSearch.jsx
@@ -1,3 +1,21 @@
+import { Box, TextField, Typography } from "@mui/material";
+import MovieCard from "../MovieList/MovieCard";
+
 export default function MoviesSearch() {
-    return <div>MoviesSearch</div>;
+    return (
+        <>
+            <Box
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+                m={3}
+            >
+                <Typography textAlign="center">Search movie bellow</Typography>
+                <TextField sx={{ width: "90%", maxWidth: "700px", m: 2 }} />
+            </Box>
+            <Box display="flex" p={3} flexWrap="wrap">
+                <MovieCard>saasdas</MovieCard>
+            </Box>
+        </>
+    );
 }
