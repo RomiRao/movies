8df364a8cfdb4d7478494c664f3acb9f7b9fce58
RomiRao <raoromina96@gmail.com>
2024-01-14 12:34:33 -0300

    movielist basic structure

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,3 +1,13 @@
+import { Pagination, Typography, Box } from "@mui/material";
+
 export default function MovieList() {
-    return <div>MovieList</div>;
+    return (
+        <>
+            <Typography>title</Typography>
+            <Box>movies</Box>
+            <Box>
+                <Pagination count={10} color="primary" />
+            </Box>
+        </>
+    );
 }
