3eb4ecc63fbf3b0c0fa8a22e06a02733a104acaa
RomiRao <raoromina96@gmail.com>
2024-01-14 21:48:29 -0300

    fixing navlinks on drawerlist

--- src/components/Navbar/NavListDrawer.jsx
+++ src/components/Navbar/NavListDrawer.jsx
@@ -5,22 +5,27 @@ import {
     ListItemButton,
     ListItemText,
 } from "@mui/material";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import styles from "./NavListDrawer.module.css";
 
 export default function NavListDrawer({ setOpen, navLinks }) {
     return (
         <Box sx={{ width: 280 }} component="nav">
             <List>
                 {navLinks.map((link) => (
-                    <ListItem
+                    <NavLink
                         key={link}
-                        disablePadding
-                        onClick={() => setOpen(false)}
+                        to={`/${link !== "Home" ? link.toLowerCase() : ""}`}
+                        className={({ isActive }) =>
+                            isActive ? `${styles.active}` : `${styles.inactive}`
+                        }
                     >
-                        <ListItemButton>
-                            <ListItemText primary={link} />
-                        </ListItemButton>
-                    </ListItem>
+                        <ListItem disablePadding>
+                            <ListItemButton sx={{ height: "70px" }}>
+                                <ListItemText primary={link} />
+                            </ListItemButton>
+                        </ListItem>
+                    </NavLink>
                 ))}
             </List>
         </Box>
