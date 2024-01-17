2c0550b3b1d6715524cd1ea8c8927f7e4fb11b45
RomiRao <raoromina96@gmail.com>
2024-01-17 15:15:44 -0300

    adding customhook api to home, still need to figure out how to do it with carousel

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,87 +1,89 @@
-import { Box, Button } from "@mui/material";
+import { Box } from "@mui/material";
 import MovieSmallList from "./MovieSmallList";
 import Carrousel from "./Carrousel";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { GoDotFill, GoDot } from "react-icons/go";
+import useMovies from "../Hooks/useMovies";
+import BarLoader from "react-spinners/BarLoader";
 
 export default function Home() {
     const [currentIndex, setCurrentIndex] = useState(0);
-    const images = [
-        {
-            src: "https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/1ecde018e863e2aaee31f00a23378c35.jpe",
-            alt: "one piece",
-            desc: "asdfasdagas",
-        },
-        {
-            src: "https://i.blogs.es/259581/one-piece/1366_2000.jpeg",
-            alt: "one piece",
-            desc: "asdasdas",
-        },
-        {
-            src: "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2018/08/one-piece_0.jpg?tf=3840x",
-            alt: "one piece",
-            desc: "asdasdas",
-        },
-    ];
+
+    const { data, getMovies } = useMovies();
+    useEffect(() => {
+        getMovies();
+    }, [currentIndex]);
 
     const goNext = () => {
-        setTimeout(() => {
-            if (currentIndex < images.length - 1) {
+        const timeout = setTimeout(() => {
+            if (currentIndex < data.length - 1) {
                 setCurrentIndex(currentIndex + 1);
-            } else if (currentIndex === images.length - 1) {
+            } else if (currentIndex === data.length - 1) {
                 setCurrentIndex(0);
             }
-            console.log("timeout", currentIndex);
         }, 5000);
-        clearTimeout(setTimeout);
+        clearTimeout(timeout);
     };
 
     goNext();
 
     const goToIndex = (index) => {
         setCurrentIndex(index);
-        console.log("index", index, currentIndex);
     };
 
     return (
         <>
-            <Box>
-                <Carrousel
-                    title={images[currentIndex].alt}
-                    desc={images[currentIndex].desc}
-                    img={images[currentIndex].src}
-                    movies={images}
-                >
-                    <Box
-                        display="flex"
-                        justifyContent="center"
-                        alignItems="center"
-                    >
-                        {images.map((movie, index) => (
-                            <Box key={index}>
-                                {index === currentIndex ? (
-                                    <GoDotFill color="white" fontSize="20px" />
-                                ) : (
-                                    <GoDot
-                                        color="white"
-                                        onClick={() => goToIndex(index)}
-                                    />
-                                )}
+            {data.length === 0 ? (
+                <Box padding={40} display="flex" justifyContent="center">
+                    <BarLoader
+                        color="#E47861"
+                        size={150}
+                        aria-label="Loading Spinner"
+                    />
+                </Box>
+            ) : (
+                <>
+                    <Box>
+                        <Carrousel
+                            title={data[currentIndex].title}
+                            desc={data[currentIndex].overview}
+                            img={data[currentIndex].poster_path}
+                        >
+                            <Box
+                                display="flex"
+                                justifyContent="center"
+                                alignItems="center"
+                            >
+                                {data.map((movie, index) => (
+                                    <Box key={index}>
+                                        {index === currentIndex ? (
+                                            <GoDotFill
+                                                color="white"
+                                                fontSize="20px"
+                                            />
+                                        ) : (
+                                            <GoDot
+                                                color="white"
+                                                onClick={() => goToIndex(index)}
+                                            />
+                                        )}
+                                    </Box>
+                                ))}
                             </Box>
-                        ))}
+                        </Carrousel>
                     </Box>
-                </Carrousel>
-            </Box>
-            <Box
-                padding={8}
-                sx={{
-                    display: { sx: "block", sm: "block", md: "flex" },
-                    justifyContent: "space-around",
-                }}
-            >
-                <MovieSmallList title="Popular movies" />
-                <MovieSmallList title="Best rated movies" />
-            </Box>
+                    <Box
+                        padding={8}
+                        sx={{
+                            display: { sx: "block", sm: "block", md: "flex" },
+                            justifyContent: "space-around",
+                        }}
+                    >
+                        <MovieSmallList title="Popular movies" />
+                        <MovieSmallList title="Best rated movies" />
+                    </Box>
+                </>
+            )}
         </>
     );
 }
