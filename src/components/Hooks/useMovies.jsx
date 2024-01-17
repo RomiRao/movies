fa11a71c66639dfa3a4f602f804116052e2eecf8
RomiRao <raoromina96@gmail.com>
2024-01-17 16:12:19 -0300

    adding individual fetch for individual movies

--- src/components/Hooks/useMovies.jsx
+++ src/components/Hooks/useMovies.jsx
@@ -24,7 +24,27 @@ const useMovies = () => {
         }
     };
 
-    return { data, getMovies };
+    const getMovie = async (id) => {
+        try {
+            const apiKey = import.meta.env.VITE_ACCESS_TOKEN;
+
+            const response = await axios.get(
+                `https://api.themoviedb.org/3/movie/${id}?language=en-US&page=1`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${apiKey}`,
+                    },
+                }
+            );
+
+            console.log("Response:", response.data);
+            setData(response.data);
+        } catch (error) {
+            console.error("Error fetching movies:", error);
+        }
+    };
+
+    return { data, getMovies, getMovie };
 };
 
 export default useMovies;
