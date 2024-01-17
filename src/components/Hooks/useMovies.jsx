1465b4b603e9df45049c814505d3dca36c969a6d
RomiRao <raoromina96@gmail.com>
2024-01-17 15:16:21 -0300

    defining function to call from api with axios

--- src/components/Hooks/useMovies.jsx
+++ src/components/Hooks/useMovies.jsx
@@ -1,13 +1,30 @@
 import { useState } from "react";
+import axios from "axios";
 
 const useMovies = () => {
     const [data, setData] = useState([]);
 
-    const getData = () => {
-        console.log(data);
+    const getMovies = async () => {
+        try {
+            const apiKey = import.meta.env.VITE_ACCESS_TOKEN;
+
+            const response = await axios.get(
+                "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
+                {
+                    headers: {
+                        Authorization: `Bearer ${apiKey}`,
+                    },
+                }
+            );
+
+            console.log("Response:", response.data.results);
+            setData(response.data.results);
+        } catch (error) {
+            console.error("Error fetching movies:", error);
+        }
     };
 
-    return { data, getData };
+    return { data, getMovies };
 };
 
 export default useMovies;
