4b51c35580b95213d4f5bd1649fcff380af83b33
RomiRao <raoromina96@gmail.com>
2024-01-18 16:01:47 -0300

    adding favorites context

---
+++ src/components/Context/FavoritesContext.jsx
@@ -0,0 +1,48 @@
+import { createContext, useEffect, useState } from "react";
+import useLocalStorage from "../Hooks/useLocalStorage";
+
+export const FavoritesContext = createContext();
+
+const FavoritesContextProvider = ({ children }) => {
+    const { get, set } = useLocalStorage();
+    const [favorites, setFavorites] = useState([]);
+
+    useEffect(() => {
+        get("favs") ? setFavorites(get("favs")) : setFavorites([]);
+    }, []);
+
+    const addFavs = (e, movie) => {
+        e.stopPropagation();
+        setFavorites([...favorites, movie]);
+        set("favs", [...favorites, movie]);
+    };
+
+    const delFavs = (e, id) => {
+        e.stopPropagation();
+        setFavorites(favorites.filter((movie) => movie.id !== id));
+        set(
+            "favs",
+            favorites.filter((movie) => movie.id !== id)
+        );
+    };
+
+    const isFavs = (id) => {
+        console.log(favorites);
+        return favorites.some((movie) => movie.id === id);
+    };
+
+    const data = {
+        favorites,
+        addFavs,
+        delFavs,
+        isFavs,
+    };
+
+    return (
+        <FavoritesContext.Provider value={data}>
+            {children}
+        </FavoritesContext.Provider>
+    );
+};
+
+export default FavoritesContextProvider;
