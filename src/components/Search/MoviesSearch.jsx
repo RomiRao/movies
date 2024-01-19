7e9568b77c34aadc085060eb933838839f34436c
RomiRao <raoromina96@gmail.com>
2024-01-19 12:14:01 -0300

    adding movie card render and search function with usemovies hook

--- src/components/Search/MoviesSearch.jsx
+++ src/components/Search/MoviesSearch.jsx
@@ -1,7 +1,18 @@
-import { Box, TextField, Typography } from "@mui/material";
+import { Box, Pagination, TextField, Typography } from "@mui/material";
 import MovieCard from "../MovieList/MovieCard";
+import { useState, useEffect } from "react";
+import { BarLoader } from "react-spinners";
+import useMovies from "../../hooks/useMovies";
 
 export default function MoviesSearch() {
+    const [input, setInput] = useState("");
+
+    const { data, searchMovie } = useMovies();
+
+    useEffect(() => {
+        searchMovie(input);
+    }, [input]);
+
     return (
         <>
             <Box
@@ -11,11 +22,59 @@ export default function MoviesSearch() {
                 m={3}
             >
                 <Typography textAlign="center">Search movie bellow</Typography>
-                <TextField sx={{ width: "90%", maxWidth: "700px", m: 2 }} />
+                <TextField
+                    sx={{ width: "90%", maxWidth: "700px", m: 2 }}
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                />
             </Box>
-            <Box display="flex" p={3} flexWrap="wrap">
-                <MovieCard>saasdas</MovieCard>
-            </Box>
+            {data.length === 0 ? (
+                <Box
+                    padding={23}
+                    display="flex"
+                    flexDirection="column"
+                    alignItems="center"
+                >
+                    <BarLoader
+                        color="#E47861"
+                        size={150}
+                        aria-label="Loading Spinner"
+                    />
+                    <Typography color="#E47861" mt={3}>
+                        The movie you are looking for doesn't exist. Please
+                        enter another title.
+                    </Typography>
+                </Box>
+            ) : (
+                <>
+                    <Box
+                        display="flex"
+                        alignItems="center"
+                        flexDirection="column"
+                    >
+                        <Box
+                            display="flex"
+                            p={3}
+                            maxWidth="87.5%"
+                            flexWrap="wrap"
+                        >
+                            {data.map((movie) =>
+                                movie.poster_path ? (
+                                    <MovieCard
+                                        key={movie.id}
+                                        title={movie.title}
+                                        img={movie.poster_path}
+                                        id={movie.id}
+                                    />
+                                ) : null
+                            )}
+                        </Box>
+                        <Box>
+                            <Pagination count={10} color="primary" />
+                        </Box>
+                    </Box>
+                </>
+            )}
         </>
     );
 }
